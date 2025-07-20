dda4ed50140ad366fa8dd58563c130aad6a2b25a
moataz-armash <mo3taz314@gmail.com>
2025-07-20 16:22:39 +0300

    Update HeroSection layout and styles; add primary color variable in globals.css

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -3,10 +3,15 @@ import Image from "next/image";
 
 export default function HeroSection() {
   return (
-    <div className="relative w-full h-dvh">
-      <Image src={heroImage} alt="Hero Image" fill />
-      <div className="absolute inset-0 flex items-center justify-center z-10">
-        <button className="bg-white text-secondary px-6 py-3 font-arabic font-semibold shadow-md cursor-pointer rounded-lg text-2xl">
+    <div className="relative w-full h-full bg-white">
+      <Image
+        src={heroImage}
+        alt="Hero Background"
+        className="object-contain md:object-cover"
+        priority
+      />
+      <div className="absolute inset-0 flex items-center justify-center z-10 mb-8">
+        <button className="bg-white text-secondary px-3 py-1 font-arabic font-semibold shadow-md cursor-pointer rounded-lg text-md md:text-2xl md:px-6 md:py-3">
           اطلب الآن
         </button>
       </div>
